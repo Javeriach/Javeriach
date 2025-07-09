b059139034ae2ba31e365415bf6cf3a457597535
Javeriach <javeriakanwal383@gmail.com>
2025-07-09 10:16:10 +0500

    add lorem

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log('Hell javeria..how are you doing...are you okax');
+console.log(
+  "I hope you are ding well and I don't know what are you doing today"
+);
---
+++ style.cs
@@ -0,0 +1,5 @@
+p{
+    color:"green"
+}
+// hgi haveria..how are youing doing it is very nice to mee tyouyadjsfkajsdlkfjalksdf
+//asjdflkasdflkajf aksdjfldsjflksdjf ksajdflaksjdflk
